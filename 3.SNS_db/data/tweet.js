938c26732b524d73deaa3f7604fcf8e505b48003
CHUL SEUNG LEE <52488182+ChulJJA@users.noreply.github.com>
2025-01-09 09:55:49 -0500

    Add files via upload

---
+++ 3.SNS_db/data/tweet.js
@@ -0,0 +1,41 @@
+import { db } from '../db/database.js';
+
+const SELECT_JOIN =
+  'SELECT tw.id, tw.text, tw.createdAt, tw.userId, users.username, users.name, users.url FROM tweets as tw JOIN users ON tw.userId=users.id';
+const ORDER_DESC = 'ORDER BY tw.createdAt DESC';
+
+export async function getAll() {
+  return db.execute(`${SELECT_JOIN} ${ORDER_DESC}`).then((result) => result[0]);
+}
+
+export async function getAllByUsername(username) {
+  return db
+    .execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username])
+    .then((result) => result[0]);
+}
+
+export async function getById(id) {
+  return db
+    .execute(`${SELECT_JOIN} WHERE tw.id=?`, [id])
+    .then((result) => result[0][0]);
+}
+
+export async function create(text, userId) {
+  return db
+    .execute('INSERT INTO tweets (text, createdAt, userId) VALUES(?,?,?)', [
+      text,
+      new Date(),
+      userId,
+    ])
+    .then((result) => getById(result[0].insertId));
+}
+
+export async function update(id, text) {
+  return db
+    .execute('UPDATE tweets SET text=? WHERE id=?', [text, id])
+    .then(() => getById(id));
+}
+
+export async function remove(id) {
+  return db.execute('DELETE FROM tweets WHERE id=?', [id]);
+}
